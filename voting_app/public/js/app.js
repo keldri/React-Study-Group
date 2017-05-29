6286427a79f7c0fee2ad69e437bc69800bd91f62
Kate eldridge <keldridge@somethingdigital.com>
2017-05-29 15:46:47 -0400

    Create child component Product & reference in ProductList

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -2,11 +2,38 @@ class ProductList extends React.Component {
   render() {
     return (
       <div className="ui unstackable items">
-        Hello, Friend I am a basic React Compnent..
+        <Product />
       </div>
     );
   }
 }
+
+
+class Product extends React.Component {
+  render() {
+    return(
+    <div className="item"> 
+        <div className="image">
+          <img src="images/products/image-aqua.png" />
+        </div>
+        <div className='middle aligned content'>
+        </div>
+        <div className='description'>
+          <a>Fort Knight</a>
+          <p> Authentic Renaissance actors....</p>
+        </div>
+        <div className="extra">
+          <span> Submitted by:</span>
+          <img 
+            className ="ui avatar image"
+            src="images/avatars/daniel.jpg"
+          />
+        </div>
+      </div>  
+    );
+  }
+}
+
 
 ReactDOM.render(
   <ProductList />,
