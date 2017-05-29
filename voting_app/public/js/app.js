e34a2655b9cdff554057b601836fce09c012be58
Kate eldridge <keldridge@somethingdigital.com>
2017-05-29 16:30:31 -0400

    Replace static data with dynamic data using props
    props passed down from parent component with props and now can replace hardcoded data

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -21,24 +21,34 @@ class ProductList extends React.Component {
 class Product extends React.Component {
   render() {
     return(
-    <div className="item"> 
-        <div className="image">
-          <img src="images/products/image-aqua.png" />
-        </div>
-        <div className='middle aligned content'>
+    <div className='item'>
+      <div className='image'>
+        <img src={this.props.productImageUrl} />
+      </div>
+      <div className='middle aligned content'>
+        <div className='header'>
+          <a>
+            <i className='large caret up icon' />
+          </a>
+          {this.props.votes}
         </div>
         <div className='description'>
-          <a>Fort Knight</a>
-          <p> Authentic Renaissance actors....</p>
+          <a href={this.props.url}>
+            {this.props.title}
+          </a>
+          <p>
+            {this.props.description}
+          </p>
         </div>
-        <div className="extra">
-          <span> Submitted by:</span>
-          <img 
-            className ="ui avatar image"
-            src="images/avatars/daniel.jpg"
+        <div className='extra'>
+          <span>Submitted by:</span>
+          <img
+            className='ui avatar image'
+            src={this.props.submitterAvatarUrl}
           />
         </div>
-      </div>  
+      </div>
+    </div>
     );
   }
 }
