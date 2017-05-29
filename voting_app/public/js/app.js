71f7ba587d3f63e88dc5becec2c58eacbdfac1a7
Kate eldridge <keldridge@somethingdigital.com>
2017-05-29 15:02:36 -0400

    Use React Dom to tell React what component to render and where

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,9 +1,14 @@
 class ProductList extends React.Component {
   render() {
     return (
-      <div className="ui unstackable items" >
+      <div className="ui unstackable items">
         Hello, Friend I am a basic React Compnent..
       </div>
     );
   }
 }
+
+ReactDOM.render(
+  <ProductList />,
+  document.getElementById('content')
+);
